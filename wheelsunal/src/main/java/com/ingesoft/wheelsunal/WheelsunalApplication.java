707cb98917ad1b9ecbdd1255e33d2437ed6a2f34
griverab <69488534+GustavoR-ivera@users.noreply.github.com>
2022-10-24 20:13:05 -0500

    Carga de proyecto

---
+++ wheelsunal/src/main/java/com/ingesoft/wheelsunal/WheelsunalApplication.java
@@ -0,0 +1,14 @@
+package com.ingesoft.wheelsunal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelsunalApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(WheelsunalApplication.class, args);
+	}
+
+}
