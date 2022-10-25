571510da23e01062940f42c264d00506bb8b99d5
NicolasLopez147 <nilopezn@unal.edu.co>
2022-10-25 03:04:20 -0500

    Proyecto

---
+++ wheelsunal/src/test/java/com/wheels/wheelsunal/WheelsunalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheels.wheelsunal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheelsunalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
