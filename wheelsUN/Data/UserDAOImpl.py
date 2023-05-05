79a16ea48e571cce769974cbdb1d0104ef0d5245
griverab <69488534+GustavoR-ivera@users.noreply.github.com>
2023-05-05 18:30:04 -0500

    complete user class

--- wheelsUN/Data/UserDAOImpl.py
+++ wheelsUN/Data/UserDAOImpl.py
@@ -74,12 +74,12 @@ class UserDAOImpl(UserDAO):
             print(f'An exception has occurred: {e}')
 
 
-    def delete(self, user_id):
+    def delete(self, user):
         try:
             with PoolCursor() as cursor:
-                query = f"DELETE " \
-                        f"FROM test_users " \
-                        f"WHERE user_id = {user_id} "
+                query = f"update test_users " \
+                        f"set deleted = 1, deleted_at = '{datetime.now()}'  " \
+                        f"where user_id = {user.user_id} "
                 cursor.execute(query)
                 print(f'deleted records: {cursor.rowcount}')
 
@@ -99,39 +99,42 @@ class UserDAOImpl(UserDAO):
         except Exception as e:
             print(f'An exception has occurred: {e}')
 
-# insert
-user1 = User(None, "Michael Jordan", "DFRG5677gh4", 0, "Belgica", "DNI", "gt5555-0", 'loquesea2@mail.com', datetime.now(),
-             '3223877590', 0, datetime.now(), datetime.now(), None, 'gt5555-0', 'generico')
-userDAO = UserDAOImpl()
-userDAO.insert(user1)
 
-# user1 = User(58, "Gerrad Lampard", "YTfr3", 1, "Inglaterra", "TI", "65665E14")
-# userDAO = UserDAOImpl()
-# userDAO.update(user1)
-#UserDAOImpl().getUsersByStatus(True)
+if __name__ == "__main__":
+    pass
+    # insert
+    #user1 = User(None, "Michael Jordan", "DFRG5677gh4", 0, "Belgica", "DNI", "gt5555-0", 'loquesea2@mail.com', datetime.now(),
+    #             '3223877590', 0, datetime.now(), datetime.now(), None, 'gt5555-0', 'generico')
+    #userDAO = UserDAOImpl()
+    # userDAO.insert(user1)
+    #
+    # user1 = User(58, "Gerrad Lampard", "YTfr3", 1, "Inglaterra", "TI", "65665E14")
+    # userDAO = UserDAOImpl()
+    # userDAO.update(user1)
+    #UserDAOImpl().getUsersByStatus(True)
 
-# #-get user by dni number
-# userDao = UserDAOImpl()
-# userDao.getUserByDni("98595-T")
+    # #-get user by dni number
+    # userDao = UserDAOImpl()
+    # userDao.getUserByDni("98595-T")
 
-# get user by id
-#userDao = UserDAOImpl()
-# check the user
-#userDao.getUserById(12)
+    # get user by id
+    #userDao = UserDAOImpl()
+    # check the user
+    #userDao.getUserById(12)
 
-# delete user
-# check the user
-#userDao.getUserById(12)
-# then you can delete it
-#userDao.delete(12)
+    # delete user
+    # check the user
+    #userDao.getUserById(12)
+    # then you can delete it
+    #userDAO.delete(user1)
 
-# # get users by status
-# userDao = UserDAOImpl()
-# userDao.getUsersByStatus(0)
+    # # get users by status
+    # userDao = UserDAOImpl()
+    # userDao.getUsersByStatus(0)
 
-# # select method
-# userDao = UserDAOImpl()
-# userDao.select()
+    # # select method
+    # userDao = UserDAOImpl()
+    # userDao.select()
 
 
-#print(datetime.now())
+    #print(datetime.now())
